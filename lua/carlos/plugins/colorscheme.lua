cdea379ba3c0ce6a2277851dc8fd4b82e1286dfe
Carlos Barbiero <carlosbarbiero@gmail.com>
2024-05-27 12:20:35 -0400

    add new plugins

--- lua/carlos/plugins/colorscheme.lua
+++ lua/carlos/plugins/colorscheme.lua
@@ -1,7 +1,20 @@
+--return {
+--  "folke/tokyonight.nvim",
+--  priority = 1000,
+--  config = function()
+--    vim.cmd("colorscheme tokyonight")
+--  end
+--}
+
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme tokyonight")
-  end
+	"navarasu/onedark.nvim",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme onedark")
+		local onedark = require("onedark")
+		onedark.setup({
+			style = "darker",
+		})
+		onedark.load()
+	end,
 }
